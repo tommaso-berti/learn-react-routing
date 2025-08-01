13951bb8c927655e3631f3c085c6b70e04adbded
Tommaso Berti <tommaso.berti@helptek.it>
2025-08-01 17:19:11 +0200

    add initial project files including components, Redux slices, and configuration

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCurrentUser, logOut } from "../features/session/sessionSlice"
+import { NavLink } from 'react-router-dom'; // Import NavLink from react-router-dom
+
+export default function Header () {
+  const currentUser = useSelector(selectCurrentUser);
+  const dispatch = useDispatch();
+
+  const handleLogout = e => {
+    dispatch(logOut())
+  }
+
+  // Replace the 4 <a> tags with <NavLink> components
+  return (
+    <div className="header">
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/articles">Articles</NavLink>
+      <NavLink to="/categories">Categories</NavLink>
+      {
+        currentUser.username
+          ? <>
+              <a href="/profile">Profile</a>
+              <button onClick={handleLogout} className="logout"> Log Out </button>
+            </>
+          : <a href="/sign-up">Sign Up</a>
+        }
+    </div>
+  )
+}
