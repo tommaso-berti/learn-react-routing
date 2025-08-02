d5a0e4396b8da5c58bfe4fdc9f7d8a104c5fe704
Tommaso Berti <tommaso.coding@gmail.com>
2025-08-02 15:52:22 +0200

    refactor routing structure to include nested routes for categories and profile editing

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -2,17 +2,17 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
 import Search from "./Search";
-import {Link , NavLink} from 'react-router-dom';
-
+import {Link , NavLink, useSearchParams} from 'react-router-dom';
 // Import Link and useSearchParams from React Router
 
 export default function Articles () {
   const articles = useSelector(selectArticles);
 
   // Grab URLSearchParams object from useSearchParams hook
-  
+  const [searchParams] = useSearchParams();
+
   // Get the queryParams from object returned from useSearchParams and set to `title`
-  const title = '';
+  const title = searchParams.get("title");
 
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
